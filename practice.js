e84c87eff8cc2be2392d4336c6e6d28dad25cc80
peastman732 <132759730+peastman732@users.noreply.github.com>
2023-07-12 17:58:46 -0400

    Validation Form
    Contact page

---
+++ practice.js
@@ -0,0 +1,89 @@
+//document.getElementById("count-el").innerText=5
+//document.getElementById("myAge").innerText=60
+
+//let count = 5
+//console.log(count)
+
+// 1. Create variable my age, and set its value to your age
+// 2. Log the myAge variable to console
+//let myAge = 65
+//console.log(count)
+
+//let firstbatch = 5
+//let secondbatch = 7
+//let count firstbatch + secondbatch 
+//console.log(count)
+
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge 
+// 3. Log myDogAge to the console
+    //let myAge = 20
+    //let humanDogRatio = 7
+
+    //let myDogAge = myAge * humanDogRatio
+    //console.log(myDogAge)
+
+//count = 15 
+//count = 15 + 1
+    //count = 15 + 1
+
+// or let count = 15
+//console.log(count)
+// count = count + 2
+// this console number will be 17 
+
+
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+// Console.log the value after each step
+
+//let bonusPoints = 50
+//console.log(bonusPoints)
+
+//bonusPoints = bonusPoints + 50
+//console.log(bonusPoints)
+
+//bonusPoints = bonusPoints - 75
+//console.log(bonusPoints)
+
+//bonusPoints = bonusPoints + 45
+//console.log(bonusPoints)
+
+
+//<!-- Create a INCREMENT button with the id="increment-btn" --> put this in html code
+function increment()
+console.log("The button was clicked")
+
+// Create a function (you decide the name) that logs out the number 42 to the console
+// Call/invoke the function
+
+//function myLogger() {
+//console.log(42) }
+//myLogger()
+
+// Create a function that logs out the sum of all the lap times
+
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+function logLapTime() {
+    // you can include with function if you want: let totalTime = lap1 + lap2 + lap3
+    console.log(lap1 + lap2 + lap3)
+}
+logLapTime()
+
+
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
+
+function incrementLap() {
+    lapsCompleted = lapsCompleted + 1
+}
+incrementLap()
+incrementLap()
+incrementLap()
+
+console.log(lapsCompleted)
+
+let count
